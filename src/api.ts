4382081b72f1e90cdc68556a2affa1d9b78fa4c8
Dhananjay singh <143948654+dhanbyte@users.noreply.github.com>
2025-07-15 15:06:06 +0530

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://photos-backend-page-cwvu.onrender.com/api",
 });
 
 export default API;
